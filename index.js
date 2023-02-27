a8ed41f1b3a3349aa6ed54eafd23d416fbcb9356
Lisa Munin <lisa.munin@unity3d.com>
2023-02-27 10:11:07 -0800

    Added url option for svgr. And updated to handle css url imports

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-module.exports = (options = {}) => ({
-    name: 'svgr',
-    setup(build) {
-        const svgr = require('@svgr/core').transform;
-        const fs = require('fs');
+const path = require("node:path");
 
-        build.onLoad({ filter: /\.svg$/ }, async (args) => {
-            const svg = await fs.promises.readFile(args.path, 'utf8');
-            const contents = await svgr(svg, { ...options }, { filePath: args.path });
-            return {
-                contents,
-                loader: options.typescript ? 'tsx' : 'jsx',
-            };
-        });
-    },
+module.exports = (options = {}) => ({
+  name: "svgr",
+  setup({ initialOptions, onResolve, onLoad, resolve }) {
+    const svgr = require("@svgr/core").transform;
+    const fs = require("fs");
+    onResolve({ filter: /\.svg$/ }, async (args) => {
+      switch (args.kind) {
+        case "import-statement":
+        case "require-call":
+        case "dynamic-import":
+        case "require-resolve":
+          return;
+        default:
+          const absPath = path.resolve(args.resolveDir, args.path);
+
+          return {
+            path: absPath,
+            external: true,
+          };
+      }
+    });
+
+    onLoad({ filter: /\.svg$/ }, async (args) => {
+      const svg = await fs.promises.readFile(args.path, "utf8");
+      const contents = await svgr(svg, { ...options }, { filePath: args.path });
+      if (args.suffix === "?url") {
+        return {
+          contents: args.path,
+          loader: "text",
+        };
+      }
+
+      return {
+        contents,
+        loader: options.typescript ? "tsx" : "jsx",
+      };
+    });
+  },
 });
