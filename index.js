304b1932aa0f09e8e94ac2d9d4b77b1474eb13e5
Lisa Munin <lisa.munin@unity3d.com>
2023-03-01 07:33:10 -0800

    Some more prettier diff updates

--- index.js
+++ index.js
@@ -24,11 +24,8 @@ module.exports = (options = {}) => ({
 
         build.onLoad({ filter: /\.svg$/ }, async (args) => {
             const svg = await fs.promises.readFile(args.path, 'utf8');
-            const contents = await svgr(
-                svg,
-                { ...options },
-                { filePath: args.path }
-            );
+            const contents = await svgr(svg, { ...options }, { filePath: args.path });
+
             if (args.suffix === '?url') {
                 return {
                     contents: args.path,
